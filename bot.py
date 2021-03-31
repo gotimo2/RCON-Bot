6db4b5d96dc3d827b7c0c97d8aa53f171bed0510
gotimo2 <61739017+gotimo2@users.noreply.github.com>
2021-03-31 22:00:08 +0200

    comments

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ global rconChannel
 
 def connectServer():
     #get rcon info from .env
-    rconAdress = (os.getenv("RCONADRESS"), int(os.getenv("RCONPORT"))) #why is this a tuple with port and adress? I DON'T KNOW, IT DOESN'T MAKE ANY SENSE!
+    rconAdress = (os.getenv("RCONADRESS"), int(os.getenv("RCONPORT"))) #why is this a tuple with port and adress, instead of RCON having another argument? I DON'T KNOW, IT DOESN'T MAKE ANY SENSE!
     rconPassword = os.getenv("RCONPASSWORD")
     global rc
     try: #remove existing connection
@@ -31,11 +31,11 @@ def connectServer():
     rc.execute("echo bot online").body.decode('utf-8', 'ignore') #print "bot online" in the console of the server
 
 @client.event
-async def on_ready():
-    rconChannelId = os.getenv("RCONCHANNELID")
-    print(f'{client.user} has connected to Discord!')
+async def on_ready(): #when the bot connects
+    rconChannelId = os.getenv("RCONCHANNELID") #get the rcon channel id
+    print(f'{client.user} has connected to Discord!') #print that the bot's connected
     global rconChannel
-    rconChannel = client.get_channel(int(rconChannelId))
+    rconChannel = client.get_channel(int(rconChannelId)) #get the rcon channel by id
 
 
 
@@ -45,21 +45,22 @@ async def on_message(message):
     global rc
     if message.author == client.user: #we don't want the bot to send commands to itself /shrug
         return
-    if message.content == 'hello bot':
+    if message.content == 'hello bot': #ping-like command
         response = f'hello human {message.author}'
         print(f'sent {response}')
         await message.channel.send(response)
-        
     if message.channel == rconChannel:
-        if message.content == "!connect":
+        if message.content == "!connect": #reconnect command
             await message.channel.send("attempting to connect to the server")
             try:
                 connectServer()
             except:
                 await message.channel.send("error connecting to the server")
-
+            #if no bug arises, the bot connected
             await message.channel.send("connected to the server")
+            #get status to print
             await message.channel.send(rc.execute("status").body.decode('utf-8', 'ignore'))
+            #if no command is done, just execute any message in the rcon channel
         else:
             await message.channel.send(rc.execute(message.content).body.decode('utf-8', 'ignore'))
     
